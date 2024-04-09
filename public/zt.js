fde09b374e7a74f1769d93968aa93a0e663b6cb6
Creeperasek <76570152+Creeperasek@users.noreply.github.com>
2024-04-09 15:52:07 +0200

    added description (contains information about type of computer) started doing sorting

--- public/zt.js
+++ public/zt.js
@@ -6,14 +6,18 @@ socket.on('connect', () => {
 })
 
 socket.on('zt_response', data => {
+  const ZtList = $('#ZT-list');
 
-  const ZtList = $('#ZT-list')
-
+    data = data.sort(function(a,b){
+      return a.name - b.name;
+    });
+    
     data.forEach(member => {
       let element = document.createElement('li')
       element.className = "list-group-item d-flex"
 
-      element.innerHTML += `<span class="me-auto" id="${member.config.id}-name">${member.name} ${member.description}</span>`
+      element.innerHTML += `<span class="me-auto" id="${member.config.id}-name">${member.name}</span>`
+      element.innerHTML += `<span class="me-auto" id="${member.config.id}-desc">${member.description}</span>`
       element.innerHTML += `<span class="me-auto" id="${member.config.id}-ipAssigment">${member.config.ipAssignments[0]}</span>`
 
       ZtList.append(element)
