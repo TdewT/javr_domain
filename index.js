984e84b385083ef16b0f3f8e857e26d553d16bcc
Creeperasek <76570152+Creeperasek@users.noreply.github.com>
2024-04-09 15:29:53 +0200

    added listners for zerotier request with checking if was requested

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const socketIO = require('socket.io');
+const axios = require('axios');
 const {statuses, types, ArmaServer, MinecraftServer, GenericServer} = require("./CustomServers");
+const ZtConfig = require('./config_zt.json');
 
 // Import information required to start a server
 const serversInfo = require('./servers_info.json')
@@ -8,6 +10,17 @@ const serversInfo = require('./servers_info.json')
 // Setup express
 const app = express()
 app.use(express.static('public'))
+
+//Setup Config for ZeroTier
+let config = {
+    method: 'GET',
+    maxBodyLength: Infinity,
+    url: 'https://api.zerotier.com/api/v1/network/0cccb752f7ccba90/member',
+    headers: { 
+      'Authorization': 'Bearer dRBMhds9vGR9Dtqcn21gmm7zFYr24iFR'
+    }
+};
+
 
 // Start server
 const server = app.listen(80, () => {
@@ -93,6 +106,33 @@ io.on('connection', client => {
         }
 
     })
+
+    
+
+    //Handeling ZeroTier Request
+    client.on('zt_request', () =>{
+        console.log("ZeroTier Request Received")
+        let wasRequested = false
+        if(!wasRequested){
+            
+            axios.request(config)
+                .then((response) => {
+                    io.emit("zt_response",response.data)
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+
+            wasRequested = true;
+        }
+        
+
+        
+
+        console.log("ZeroTier Response sent")
+    })
+
+    
 })
 
 // Define all servers
