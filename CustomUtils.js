18d91df8215b4d5b9f35910ff8bd8e5b1f098dc6
TdewT <szwej.jakub@gmail.com>
2024-04-21 15:30:51 +0200

    Some changes that didn't push
    CustomUtils.js:
    - Moved extractNums to CustomUtils.js

--- CustomUtils.js
+++ CustomUtils.js
@@ -18,6 +18,36 @@ function removeDuplicateSpace(string) {
     return string.replace(/\s\s+/g, ' ');
 }
 
+function extractNums(data) {
+    let res;
+    if (typeof data === "string") {
+        res = '';
+        for (const char of data) {
+            if (char >= '0' && char <= '9') {
+                res += char;
+            }
+        }
+        return Number(res)
+    }
+    else if (typeof data === "object") {
+        res = [];
+        for (let i = 0; i < data.length; i++) {
+            let tmp = '';
+            for (const char of data[i]) {
+                if (char >= '0' && char <= '9') {
+                    tmp += char;
+                }
+            }
+            if (tmp !== '')
+                res.push(tmp);
+        }
+        return res
+    }
+    else{
+        throw new Error('Function "extractNums()" only takes string or object type arguments');
+    }
+}
+
 module.exports = {
     killTask,
     removeDuplicateSpace,
