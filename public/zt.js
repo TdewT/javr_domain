965d92769b69554ec036ebd470fc82a1026b7d69
TdewT <szwej.jakub@gmail.com>
2024-04-22 15:03:42 +0200

    Fixed frontend
    - Fixed issue #6
    - Adjusted display of servers on zt.html
    - Fixed links and names on navbar
    - Added missing semicolons in zt
    - Fixed script link for zt.js

--- public/zt.js
+++ public/zt.js
@@ -1,9 +1,9 @@
 const $ = (e) => document.querySelector(e);
-const socket = io('ws:///')
+const socket = io('ws:///');
 
 socket.on('connect', () => {
   socket.emit('zt_request')
-})
+});
 
 socket.on('zt_response', data => {
   const ZtList = $('#ZT-list');
@@ -12,16 +12,16 @@ socket.on('zt_response', data => {
     
 
     data.forEach(member => {
-      let element = document.createElement('li')
-      element.className = "list-group-item d-flex"
+      let element = document.createElement('li');
+      element.className = "list-group-item d-flex";
 
-      element.innerHTML += `<span class="me-auto" id="${member.config.id}-name">${member.name}</span>`
-      element.innerHTML += `<span class="me-auto" id="${member.config.id}-desc">${member.description}</span>`
-      element.innerHTML += `<span class="me-auto" id="${member.config.id}-ipAssigment">${member.config.ipAssignments[0]}</span>`
+      element.innerHTML += `<span class="w-25" id="${member.config.id}-name">${member.name}</span>`;
+      element.innerHTML += `<span class="me-auto" id="${member.config.id}-desc">${member.description}</span>`;
+      element.innerHTML += `<span class="ms-auto" id="${member.config.id}-ipAssigment">${member.config.ipAssignments[0]}</span>`;
 
       ZtList.append(element)
     })
-})
+});
 
 //TODO: Sort By desc
 function compareByName( a, b ) {
