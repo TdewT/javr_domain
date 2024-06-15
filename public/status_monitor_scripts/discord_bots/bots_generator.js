6fbb23e9ed1899706159846c3b5f2dbe41e39b2a
TdewT <73232730+TdewT@users.noreply.github.com>
2024-06-15 16:28:59 +0200

    Added frontend script for handling bots

---
+++ public/status_monitor_scripts/discord_bots/bots_generator.js
@@ -0,0 +1,31 @@
+function generateDiscordBots(bots){
+    // Go through all passed bots
+    for (const bot of bots) {
+        // Check if bot was already loaded
+        if (!$(`#${bot.htmlID}-bot-status-box`)){
+            printBot(bot);
+        }
+        else{
+            updateBot(bot);
+        }
+    }
+}
+
+function printBot(bot){
+    // Create element and set it's classes
+    const botElement = document.createElement("li");
+    botElement.className = "list-group-item d-flex";
+
+    // Display status
+    botElement.innerHTML +=
+        `<div id="${bot.htmlID}-bot-status-box" class="bot-status-box me-auto flex">` +
+        `<span class="me-4" id="${bot.htmlID}-status">${statusIndicators[bot.status]}</span>` +
+        `<span class="me-4" id="${bot.htmlID}-status-text">${getStatusText(bot)}</span>` +
+        `</div>`;
+
+    // Display name
+    botElement.innerHTML +=
+        `<span class="me-name">${bot.displayName}</span>`;
+
+    return botElement;
+}
