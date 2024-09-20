b1caac3755a42404d7a478dcba7417c8c42c1531
TdewT <szwej.jakub@gmail.com>
2024-09-20 17:07:17 +0200

    Added comments to `_document.jsx`

--- Website/src/pages/_document.jsx
+++ Website/src/pages/_document.jsx
@@ -5,11 +5,13 @@ export default function _document() {
         <>
             <Html lang={'en'} data-bs-theme="dark">
                 <Head>
+                    {/* Bootstrap styles */}
                     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
                           rel="stylesheet"
                           integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
                           crossOrigin="anonymous"/>
 
+                    {/* Load google fonts*/}
                     <link rel="preconnect" href="https://fonts.googleapis.com"/>
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin = "anonymous"/>
                     <link href="https://fonts.googleapis.com/css2?family=Jersey+25&display=swap" rel="stylesheet"/>
@@ -17,6 +19,8 @@ export default function _document() {
                 <body>
                     <Main/>
                     <NextScript/>
+
+                    {/* Bootstrap scripts */}
                     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
                             integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
                             crossOrigin="anonymous"
