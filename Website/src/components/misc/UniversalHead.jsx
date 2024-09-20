0ae290ab4fcff73d7b536f9854681f7837f58e18
TdewT <szwej.jakub@gmail.com>
2024-09-20 17:40:39 +0200

    Removed unnecessary meta elements

--- Website/src/components/misc/UniversalHead.jsx
+++ Website/src/components/misc/UniversalHead.jsx
@@ -2,14 +2,13 @@ import Head from 'next/head';
 import Link from "next/link";
 import {Meta} from "next/dist/lib/metadata/generate/meta";
 
-function UniversalHead() {
+function UniversalHead({children}) {
     return (
         <>
             <Head>
                 <title>JAVR Domain</title>
-                <meta charSet="UTF-8"/>
-                <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 <link rel="icon" type="image/x-icon" href="/img/favicon.ico"/>
+                {children}
             </Head>
         </>
     )
