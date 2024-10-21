557a9f5dc001a344c3bd7b368a3f7b10cca32397
TdewT <szwej.jakub@gmail.com>
2024-10-21 16:25:43 +0200

    Removed unused functions from ServerManager's events

--- server_manager/src/lib/SocketEvents.js
+++ server_manager/src/lib/SocketEvents.js
@@ -40,54 +40,6 @@ class SocketEvents {
     static requestNotAllowed(websocket) {
         websocket.emit(Events.REQUEST_FAILED, "Ta funkcja została wyłączona w configu");
     }
-
-    /**
-     * @desc Sends ZeroTier data to client.
-     * @param websocket - Socket.io websocket, over which the data will be sent.
-     * @param {JSON} data - data from ZeroTier's API.
-     */
-    static ztResponse(websocket, data) {
-        websocket.emit(Events.ZT_RESPONSE, data)
-    }
-
-    /**
-     * @desc Sends status request on provided websocket
-     * @param websocket - Socket.io websocket, over which the request will be sent.
-     */
-    static statusRequest(websocket) {
-        websocket.emit(Events.STATUS_REQUEST);
-    }
-
-    /**
-     * @desc Sends a request to stop a server to provided server manager's socket.
-     * @param websocket - Socket.io websocket, over which the request will be sent.
-     * @param serverID - HtmlID of the server.
-     * @param clientSocketID - ID of the socket of the user who sent the request.
-     */
-    static stopServerRequest(websocket, serverID, clientSocketID) {
-        websocket.emit(Events.STOP_SERVER_REQUEST, serverID, clientSocketID);
-    }
-
-    /**
-     * @desc Sends a request to start a Discord bot to provided server manager's socket.
-     * @param websocket - Socket.io websocket, over which the request will be sent.
-     * @param botID - HtmlID of the bot.
-     * @param clientSocketID - ID of the socket of the user who sent the request.
-     */
-    static startDBotRequest(websocket, botID, clientSocketID) {
-        websocket.emit(Events.START_DBOT_REQUEST, botID, clientSocketID);
-    }
-
-    /**
-     * @desc Sends a request to stop a Discord bot to provided server manager's socket.
-     * @param websocket - Socket.io websocket, over which the request will be sent.
-     * @param botID - HtmlID of the bot.
-     * @param clientSocketID - ID of the socket of the user who sent the request.
-     */
-    static stopDBotRequest(websocket, botID, clientSocketID) {
-        websocket.emit(Events.STOP_DBOT_REQUEST, botID, clientSocketID);
-    }
-
 }
 
 module.exports = SocketEvents;
