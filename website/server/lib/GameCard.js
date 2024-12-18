06f6f891b20c4c73c8b7346af13001f8135063d8
TdewT <szwej.jakub@gmail.com>
2024-12-18 13:42:18 +0100

    Moved components and `GameCard` class to proper folders

---
+++ website/server/lib/GameCard.js
@@ -0,0 +1,10 @@
+export default class GameCard {
+
+    constructor(name, optimalPlayers, maxPlayers, minPlayers, icon) {
+        this.name = name;
+        this.optimalPlayers = optimalPlayers;
+        this.maxPlayers = maxPlayers;
+        this.minPlayers = minPlayers;
+        this.icon = icon;
+    }
+}
