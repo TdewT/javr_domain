8f93b5cc79fec7179aa74cacf37cbc865d70abee
TdewT <szwej.jakub@gmail.com>
2024-12-24 15:30:12 +0100

    Updated `GameCardList`

--- website/server/lib/GameCardList.js
+++ website/server/lib/GameCardList.js
@@ -1,17 +1,29 @@
-let {gameCards, allUsersGameCards} = require("@server-lib/globals.js");
+let {allUsersGameCards, setGameCards, getGameCards} = require("@server-lib/globals.js");
+const GameCard = require("@server-lib/GameCard.js");
+const SocketEvents = require("@server-lib/SocketEvents.cjs");
 
 class GameCardList {
-    static addGameCard(gameCard) {
-        gameCards.push(gameCard);
+    static addGameCard(card) {
+        const gameCards = getGameCards();
+        gameCards.push(new GameCard(card));
+        SocketEvents.gameCardsResponse();
     }
 
-    static removeGameCard(gameCard) {
-        gameCards = gameCards.filter(el => el.name !== gameCard.name)
+    static removeGameCard(card) {
+        setGameCards(getGameCards().filter(item => item.id !== card.id))
+        SocketEvents.gameCardsResponse();
+    }
+
+    static updateGameCard(card){
+        const cardIndex = getGameCards().findIndex(item => item.id === card.id);
+        if (cardIndex !== -1){
+            getGameCards()[cardIndex] = card;
+        }
+        SocketEvents.gameCardsResponse();
     }
 
     static updateUsersList(user, cardList) {
         allUsersGameCards[user] = cardList;
-        console.log(allUsersGameCards[user]);
     }
 }
 
